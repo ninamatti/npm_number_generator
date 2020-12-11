9dac1d691a354677629c45ce51060a838867f911
Nina Matti <ninadesireematti@gmail.com>
2020-12-11 10:42:03 +0900

    created first test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,10 @@
 const { expect } = require("chai");
 const { randomNoGenerator } = require("../index.js");
 
-
-
+describe("randomNoGenerator", function () {
+    it("should return an integer that is between the given min and max values", () => {
+      const rando = randomNoGenerator(5, 10);
+      expect(rando).to.be.above(4);
+      expect(rando).to.be.below(11);
+    });
+});
